d2d6747d97def51861dcdecaf3c7cb1f3f1f4bd6
emirogs <emrehamurcu_1995@hotmail.com>
2018-12-10 17:33:28 +0300

    first commit

---
+++ tostcu/src/main/java/com/emrehmrc/tostcu/Deneme.java
@@ -0,0 +1,11 @@
+package com.emrehmrc.tostcu;
+
+import android.util.Log;
+
+public class Deneme {
+    private static final String TAG = "Deneme";
+
+    public static void d(String msg) {
+        Log.d(TAG, msg);
+    }
+}
